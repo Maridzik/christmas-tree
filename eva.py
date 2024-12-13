cce426aa01e67c4b4fa282ba5a100f3cab5498c6
Ева <eva56m@yandex.ru>
2024-12-13 16:49:07 +0500

    arguments update

--- eva.py
+++ eva.py
@@ -2,7 +2,7 @@ import pygame
 from random import randrange
 
 
-def eva_paint(screen, pos):
+def eva_paint(screen, x, y):
     mylist = ['игрушка11.png', 'игрушка21.png']
     imp = pygame.image.load(mylist[randrange(0, 2)])
-    screen.blit(pygame.transform.scale(imp, (30, 30)), (pos[0] - 15, pos[1]))
+    screen.blit(pygame.transform.scale(imp, (30, 30)), (x - 15, y))
